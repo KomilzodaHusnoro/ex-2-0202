9b370762a5734defb42f6c3a7532df9559be567f
Husnoro <Husnoro>
2020-04-05 19:35:25 +0500

    без ввода в Main

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Rectangle 
+    {
+        double side1, side2, perimeter, area;
+        public void Perimeter (double side1, double side2) 
+        {
+            perimeter = (side1 + side2)*2;
+        }
+        public void Area (double side1, double side2)
+        {
+            area = side1*side2;
+        }
+    }
+}
